a891f8e27ab976dfb5fb531b836ff11a07b64587
Eater-Git <31100115+Eater-Git@users.noreply.github.com>
2025-04-26 00:15:16 +0900

    protocの改良

--- compile_proto.bash
+++ compile_proto.bash
@@ -1,7 +1,8 @@
 # 例 (protoファイルがprotoディレクトリにある場合)
 mkdir proto
 # .proto ファイルを proto/ ディレクトリに配置
-protoc --python_out=. proto/*.proto
+# protoc --python_out= proto/*.proto --pyi_out=
+protoc --proto_path=ssl-game-controller/proto/ --python_out=./proto/ --pyi_out=./proto/ ssl-game-controller/proto/*/*.proto
 # 同じ階層に ssl_gc_referee_message_pb2.py などが生成される
 # これらを proto/__init__.py などで管理するか、
 # 各スクリプトで from proto import ... のように参照
